12bc683a5dafee34eae93e020f5ebb57628e3d56
benkaan001 <benkaan001@gmail.com>
2022-06-27 00:17:39 -0500

    update Challenge58

--- JavaTalk/src/javatalk/Challenge58.java
+++ JavaTalk/src/javatalk/Challenge58.java
@@ -1,6 +1,13 @@
 package javatalk;
 
+
 public class Challenge58 {
+	
+	// create a Java method that rotates the first and last 4 digits to match the first 4 digits with the last for digits.
+	// i.e. 10-13-0131 -> first4=1013 && last4=0131 -> since we can get last4 by rotating first4 or vice versa, this passess.
+	// if the digits meet the criteria, add it to the supplied output array.
+	// if not, replace the last4 digit with "Nope"
+	// return/print the output. 
 	
 	public void getAnswer() {
 		
@@ -11,18 +18,41 @@ public class Challenge58 {
 		// expected output 
 		// ["0131","5034","Nope"]
 		
-		String currentStr = "";
 		String first4 ="";
 		String last4="";
 		
 		for(int i=0; i<stringValues.length;i++) {
-			currentStr+=stringValues[i].split("-")+"";
-			first4 = currentStr.substring(0, 4);
-			last4 = currentStr.substring(4,8);
-//			System.out.println(first4);
+			
+			first4 = stringValues[i].replace("-", "").substring(0,4);
+			last4 = stringValues[i].replace("-","").substring(4,8);
+			
+			char first4nums [] = first4.toCharArray();
+			char last4nums [] = last4.toCharArray();
+			
+						
+			int sum1 =0;
+			int sum2=0;
+			
+			for(int j=0; j<4; j++) {
+				sum1+=(int)(first4nums[j]);
+				sum2+=(int)(last4nums[j]);
+			}
+		
+			
+			if(sum1==sum2) {
+				output[i] = last4;
+			}
+			else {
+				output[i] = "Nope";
+			}
 		
 		
 		
+		}
+		for(int i=0; i< output.length; i++) {
+			
+			System.out.print(output[i]+" ");
+		}
 		
 		
 		
